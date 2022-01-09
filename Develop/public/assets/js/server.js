174c4c51dc6a3d57124560a4a83623c495f274d0
Acoleman37 <aleccoleman123@yahoo.com>
2022-01-08 16:46:13 -0800

    added front end routes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,21 +1,45 @@
 const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 const notes = require('./db/db');
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return newNote;
+}
 
 app.get('api/notes', (req, res) => {
     res.json(notes);
 });
 
 app.post('api/notes', (req, res) => {
-    console.log('req.body');
-    res.json(req.body);
-})
+    req.body.id = allNotes.length.toString();
+
+    const newNote = createNewNote(req.body, notes);
+    res.json(notes);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
