8983e0e5b49b9aa8a9bf8a70ef446f4035003108
Acoleman37 <aleccoleman123@yahoo.com>
2022-01-08 16:18:24 -0800

    set up express server

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+
+app.get('api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
