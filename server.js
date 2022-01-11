1b180eee46d52a840af848d80e845ddf27c16df3
Acoleman37 <aleccoleman123@yahoo.com>
2022-01-10 17:28:13 -0800

    Add Heroku

--- server.js
+++ server.js
@@ -15,37 +15,26 @@ app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 function createNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
-        notesArray = [];
+    notesArray = [];
     
     if (notesArray.length === 0)
-        notesArray.push(0);
-
+    notesArray.push(0);
+    
     body.id = notesArray[0];
     notesArray[0]++;
-
+    
     notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
+        );
+        return newNote;
 }
-
+    
 app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
@@ -54,22 +43,34 @@ app.post('/api/notes', (req, res) => {
 function deleteNote(id, notesArray) {
     for (let i = 0; i < notesArray.length; i++) {
         let note = notesArray[i];
-
+        
         if (note.id == id) {
             notesArray.splice(i, 1);
             fs.writeFileSync(
                 path.join(__dirname, './db/db.json'),
                 JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
+                );
+                
+                break;
+            }
         }
-    }
 }
-
+        
 app.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, allNotes);
     res.json(true);
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
